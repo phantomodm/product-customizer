0e8e5551811a4cec292e5d25e269c4681aa94d23
PhantomOdm <31314894+phantomodm@users.noreply.github.com>
2019-05-17 13:22:03 -0300

    MasterBuild

---
+++ build-script.js
@@ -0,0 +1,13 @@
+const fs = require('fs-extra');
+const concat = require('concat');
+
+(async function build(){
+    const files = [
+        './dist/inline.bundle.js',
+        './dist/polyfills.bundle.js',
+        './dist/main.bundle.js'
+    ]
+
+    await fs.ensureDir('nystix-ui')
+    await concat(files,'app-name.js')
+})
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,68 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule, Injector } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule} from '@angular/fire/database';
+import {
+  MatIconModule,
+  MatInputModule,
+  MatCardModule,
+  MatRadioModule,
+  MatCheckboxModule,
+  MatStepperModule,
+  MatSliderModule,
+  MatDividerModule,
+  MatButtonModule,
+  MatSnackBarModule,
+  MatSlideToggleModule,
+  MatProgressSpinnerModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './main/main.component';
+
+const firebase = {
+  "apiKey":'',
+  "authDomain":'',
+  "databaseURL": '',
+  "projectId":'',
+  "storageBucket":'',
+  "messagingSenderId":''
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(firebase),
+    AngularFireDatabaseModule,
+    MatIconModule,
+    MatInputModule,
+    MatCardModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatStepperModule,
+    MatSliderModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatSlideToggleModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MainComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector:Injector){
+    const el = createCustomElement(MainComponent,{injector: this.injector})
+    customElements.define('name-of-app', el)
+  }
+ }
